34b19a4cc5056247129edf504fe220d3409ee12c
Aleksandr Tsertkov <aleksandr@crytek.com>
2014-06-14 03:38:08 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,80 @@
+# exec-sh
+
+Execute shell command forwarding all stdio streams.
+
+[![Build Status](https://travis-ci.org/tsertkov/exec-sh)](https://travis-ci.org/tsertkov/exec-sh)
+[![NPM version](https://badge.fury.io/js/exec-sh.png)](http://badge.fury.io/js/exec-sh)
+[![David Status](https://david-dm.org/tsertkov/exec-sh.png)](https://david-dm.org/tsertkov/exec-sh) 
+
+---
+
+## Showcase
+```
+// JavaScript
+
+execSh("echo hello exec-sh && bash", { cwd: "/home" }, function(err){
+  if (err) {
+    console.log("Exit code: ", err.code);
+  }
+});
+```
+
+```
+// Terminal output: interactive bash session
+
+hello exec-sh
+bash-3.2$ pwd
+/home
+bash-3.2$ exit 99
+exit
+Exit code:  99
+```
+
+## Features
+
+exec-sh is a wrapper for child_process.spawn with improvements:
+
+- Cross platform command execution:
+  - Windows: `cmd /C COMMAND`
+  - others: `sh -c COMMAND`
+- Fowrards all stdio streams to current terminal (by default):
+  - try `execSh("bash")`
+  - try `execsh("echo -n Say: && read i && echo Said:$i")`
+- stdout and stderr are passed to callback when available
+  - try `execSh("pwd", console.log)`
+
+## Installation
+
+`npm install exec-sh`
+
+## Usage
+
+```
+var execSh = require("../");
+
+// run interactive bash shell
+execSh("echo lorem && bash", { cwd: "/home" }, function(err){
+  if (err) {
+    console.log("Exit code: ", err.code);
+    return;
+  }
+
+  // collect streams output
+  var child = execSh(["bash -c id", "echo lorem >&2"], true,
+    function(err, stdout, stderr){
+      console.log("error: ", err);
+      console.log("stdout: ", stdout);
+      console.log("stderr: ", stderr);
+    });
+});
+```
+
+## Testing
+
+`npm test`
+
+## License
+
+The MIT License (MIT)
+
+[![Bitdeli Badge](https://d2weczhvl823v0.cloudfront.net/tsertkov/exec-sh/trend.png)](https://bitdeli.com/free "Bitdeli Badge")
---
+++ test/exec-sh.js
@@ -0,0 +1,120 @@
+/*global describe, it, before, beforeEach, after, afterEach */
+var
+  execSh = require("../"),
+  assert = require("assert"),
+  sinon = require("sinon"),
+  merge = require("merge"),
+  cp = require("child_process");
+
+describe("exec-sh", function(){
+
+  describe("module.exports", function(){
+    it("should export a single function", function(){
+      assert.equal(typeof execSh, "function");
+    });
+  });
+
+  describe("#execSh() arguments", function(){
+    var spawn, exitCode;
+
+    beforeEach(function(){
+      exitCode = 0;
+      spawn = sinon.stub(cp, "spawn");
+      spawn.returns({
+        spawn_return: true,
+        on: function(e, c){
+          if (e === "close") {
+            c(exitCode);
+          }
+        }
+      });
+    });
+
+    afterEach(function(){
+      cp.spawn.restore();
+    });
+
+    it("should pass command to spawn function", function(){
+      execSh("command");
+      sinon.assert.calledOnce(spawn);
+      assert.equal("command", spawn.getCall(0).args[1][1]);
+    });
+
+    it("should accept array of commands to run", function(){
+      execSh(["command1", "command2"]);
+      sinon.assert.calledOnce(spawn);
+      assert.equal("command1;command2", spawn.getCall(0).args[1][1]);
+    });
+
+    it("should accept true as options argument", function(){
+      execSh("command", true);
+      sinon.assert.calledOnce(spawn);
+      assert.equal(spawn.getCall(0).args[2].stdio, null);
+    });
+
+    it("should merge defaults with options", function(){
+      execSh("command");
+      var defOptionsClone = merge(true, spawn.getCall(0).args[2]);
+      var options = { key: "value" };
+
+      execSh("command", options);
+      assert.deepEqual(spawn.getCall(1).args[2], merge(true, defOptionsClone, options));
+
+      // change value of the fist property in default options to null
+      assert.ok(Object.keys(defOptionsClone).length > 0);
+      defOptionsClone[Object.keys(defOptionsClone)[0]] = null;
+
+      execSh("command", defOptionsClone);
+      assert.deepEqual(spawn.getCall(2).args[2], defOptionsClone);
+    });
+
+    it("should accept optional 'callback' parameter", function(){
+      var callback = sinon.spy();
+      execSh("command", callback);
+      execSh("command", { key: "value" }, callback);
+      sinon.assert.callCount(callback, 2);
+    });
+
+    it("should use 'cmd /C' command prefix on windows", function(){
+      var platform = process.platform;
+      process.platform = "win32";
+      execSh("command");
+      process.platform = platform;
+
+      sinon.assert.calledOnce(spawn);
+      assert.equal(spawn.getCall(0).args[0], "cmd");
+    });
+
+    it("should use 'sh -c' command prefix on *nix", function(){
+      var platform = process.platform;
+      process.platform = "linux";
+      execSh("command");
+      process.platform = platform;
+
+      sinon.assert.calledOnce(spawn);
+      assert.equal(spawn.getCall(0).args[1][0], "-c");
+      assert.equal(spawn.getCall(0).args[0], "sh");
+    });
+
+    it("should return spawn() result", function(){
+      assert(execSh("command").spawn_return);
+    });
+
+    it("should catch exceptions thrown by spawn", function(done){
+      spawn.throws();
+      execSh("command", function(err){
+        assert(err instanceof Error);
+        done();
+      });
+    });
+
+    it("should run callback with error when shell exit with non-zero code", function(done){
+      exitCode = 1;
+      execSh("command", function(err){
+        assert(err instanceof Error);
+        assert.equal(exitCode, err.code);
+        done();
+      });
+    });
+  });
+});
