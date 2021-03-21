9beff8be92eeac96f613d43272d413cac3260563
Nikolay Lapshin <nlapshin1989@gmail.com>
2021-03-21 13:35:53 +0300

    Created namespace for execSh function

--- types/index.d.ts
+++ types/index.d.ts
@@ -2,10 +2,12 @@ declare module 'exec-sh' {
   import { SpawnOptionsWithoutStdio, ChildProcessWithoutNullStreams } from 'child_process';
 
   export type execShCallback = (error: Error | null, stdout: string, stderr: string) => void;
-  export type execShThis = {
-    promise(command: string | string[], options: SpawnOptionsWithoutStdio, callback: execShCallback): ChildProcessWithoutNullStreams;
+
+  declare function execSh (command: string | string[], options?: SpawnOptionsWithoutStdio | boolean, callback?: execShCallback): ChildProcessWithoutNullStreams;
+  declare function execSh (command: string | string[], callback?: execShCallback): ChildProcessWithoutNullStreams;
+  declare namespace execSh {
+    export const promise: (command: string | string[], options?: SpawnOptionsWithoutStdio) => Promise<{ stdout: string, stderr: string }>;
   }
 
-  declare function execSh (this: execShThis, command: string | string[], options: SpawnOptionsWithoutStdio, callback: execShCallback): ChildProcessWithoutNullStreams;
-  declare function execSh (this: execShThis, command: string | string[], callback: execShCallback): ChildProcessWithoutNullStreams;
+  export default execSh;
 }
