19811ab94cd7cf455b41e3684b77465de74ce86d
Aleksandr Tsertkov <tsertkov@gmail.com>
2021-03-24 12:03:25 +0100

    Do not use module in type declaration

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,22 +1,20 @@
-declare module 'exec-sh' {
-  import { ChildProcess } from 'child_process';
+import { ChildProcess } from 'child_process';
 
-  declare function execSh (
+declare function execSh(
+  command: string | string[],
+  options?: object | true,
+  callback?: (
+    error: Error | null,
+    stdout: string,
+    stderr: string
+  ) => void
+): ChildProcess;
+
+declare namespace execSh {
+  declare function promise(
     command: string | string[],
-    options?: object | true,
-    callback?: (
-      error: Error | null,
-      stdout: string,
-      stderr: string
-    ) => void
-  ): ChildProcess;
+    options?: object | true
+  ): Promise<{ stdout: string, stderr: string }>;
+};
 
-  declare namespace execSh {
-    declare const promise: (
-      command: string | string[],
-      options?: object | true
-    ) => Promise<{ stdout: string, stderr: string }>;
-  }
-
-  export default execSh;
-}
+export = execSh;
